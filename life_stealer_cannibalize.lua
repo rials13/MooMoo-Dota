43eb9b0c6038f4530087b903bdf117ea52b369f8
Riley <yallol7@hotmail.com>
2021-02-03 10:17:08 -0800

    Add files via upload

---
+++ life_stealer_cannibalize.lua
@@ -0,0 +1,11 @@
+life_stealer_cannibalize = class({})
+LinkLuaModifier( "modifier_life_stealer_cannibalize", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function life_stealer_cannibalize:GetIntrinsicModifierName()
+    return "modifier_life_stealer_cannibalize"
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
