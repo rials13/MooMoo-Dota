7f3ac884129d72db1e87943aac93b8f09e841dc4
Riley <yallol7@hotmail.com>
2022-04-12 14:56:18 -0700

    Delete items directory

--- items/item_halberd_axe.lua
+++
@@ -1,8 +0,0 @@
-item_halberd_axe = class({})
-LinkLuaModifier( "item_halberd_axe_modifier", "items/item_halberd_axe_modifier", LUA_MODIFIER_MOTION_NONE )
-
---------------------------------------------------------------------------------
--- Passive Modifiers
-function item_halberd_axe:GetIntrinsicModifierName()
-    return "item_halberd_axe_modifier"
-end
